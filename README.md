6e38f3077cca759816031d7954dc7b3418bafe64
Dawid2849 <dawid2849@gmail.com>
2024-01-30 12:26:30 +0100

    19.03.35

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 ## YouTube ReVanced
 
-YouTube ReVanced package by HuskyDG
+YouTube ReVanced package by Dawid2849.
 
 ### Background
 
-- Dynamic mount YouTube ReVanced APK and make bind mount only be visible for YouTube App to avoid detection, other app will only see the original `base.apk` of YouTube.
-- Dynamic patch Play Store everytime opening app to ensure YouTube is always detached
+Dynamic mount YouTube ReVanced APK and make bind mount only be visible for YouTube App to avoid detection, other app will only see the original `base.apk` of YouTube.
+This package doesn't contain detach function like the orignal package did. Please use some other module for detach instead (I use [zygisk-detach](https://github.com/j-hc/zygisk-detach)).
+
+### Requirements
+
+- [Process Monitor Tools](https://github.com/HuskyDG/magic_proc_monitor) module with version 2.1+ (2.3 is recommended as it's the latest version and it supports [KSU](https://github.com/tiann/KernelSU))
+- normal YouTube already installed (version has to be the same as the one in this module)
+
+### Credits
+
+- [HuskyDG](https://github.com/HuskyDG/) - original script for dynamic mount
--- customize.sh
+++ customize.sh
@@ -3,12 +3,10 @@
 
 if [ $BOOTMODE = false ]; then
 	ui_print "- Installing through TWRP Not supported"
-	ui_print "- Intsall this module via Magisk Manager"
+	ui_print "- Intsall this module via your KSU/Magisk Manager"
 	abort "- Aborting installation !!"
 fi
 api_level_arch_detect
-
-MAGISKTMP="$(magisk --path)" || MAGISKTMP=/sbin
 
 PKGNAME="$(grep_prop package "$MODPATH/module.prop")"
 APPNAME="YouTube"
@@ -24,7 +22,7 @@ then
 	abort "- Aborting installation !!"
 fi
 
-[ ! -d "$MAGISKTMP/.magisk/modules/magisk_proc_monitor" ] && {
+[ ! -d "/data/adb/modules/magisk_proc_monitor" ] && {
     MURL=http://github.com/HuskyDG/magisk_proc_monitor
     ui_print "- Process monitor tool is not installed"
     ui_print "  Please install it from $MURL"
@@ -40,12 +38,6 @@ then
   am start -a android.intent.action.VIEW -d "$URL" &>/dev/null
 	abort "- Aborting installation !!"
 fi
-
-ui_print "- Install sqlite3 plug-in for detach"
-mkdir "$MODPATH/bin"
-unzip -oj "$MODPATH/sqlite3.zip" "$ABI/sqlite3" -d "$MODPATH/bin" &>/dev/null || abort "! Unzip failed"
-chmod 755 "$MODPATH/bin/sqlite3"
-rm -rf "$MODPATH/sqlite3.zip"
 
 # Disable battery optimization for YouTube ReVanced
 sleep 1
